6ef9501a87efc2c4279a9b2ebe92d8e4be537661
zhoufeiy <zhoufeiy@usc.edu>
2024-05-16 14:05:15 -0700

    Add tool file and tamplate.

--- setup_sequences.py
+++ setup_sequences.py
@@ -1,4 +1,4 @@
 from blossom_sequence_comb import combine_sequences
 
 # If this throws error, create cognitive directory under corresponding directory.
-combine_sequences(["yes", "no"], "yes_no")
+combine_sequences(["yes", "no", "grand/grand1"], "yes_no")
