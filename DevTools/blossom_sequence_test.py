5dfbf1876212c8e8620544e2d0222d0e2403dc81
zhoufeiy <zhoufeiy@usc.edu>
2024-05-17 14:47:34 -0700

    Move blossom sequence testing tool to DevTools dir.

--- DevTools/blossom_sequence_test.py
+++ DevTools/blossom_sequence_test.py
@@ -1,9 +1,9 @@
 import sys
 
-sys.path.append("./blossom-public")
+sys.path.append("../blossom-public")
 from blossompy import Blossom
 
-bl = Blossom(sequence_dir='./blossom-public/blossompy/src/sequences')
+bl = Blossom(sequence_dir='../blossom-public/blossompy/src/sequences')
 bl.connect()  # safe init and connects to blossom and puts blossom in reset position
 bl.load_sequences()
 print("++++++++++++++++++ Initialization Complete ++++++++++++++++++")
