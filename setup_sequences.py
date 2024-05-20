4bcb8a97860dfa4616988d10c61d9e89ce4a0471
zhoufeiy <zhoufeiy@usc.edu>
2024-05-20 16:33:36 -0700

    Add blossom sequences setup and backup.

--- setup_sequences.py
+++ setup_sequences.py
@@ -1,4 +1,19 @@
 from blossom_sequence_comb import combine_sequences
 
 # If this throws error, create cognitive directory under corresponding directory.
-combine_sequences(["yes", "no", "grand/grand1"], "yes_no")
+combine_sequences(["grand/grand1", "anger/anger_0_109", "yes", "anger/anger_scream"], "01")
+
+combine_sequences(["amazon_demo/Introduction_NameAsking", "amazon_demo/Introduction_MakeFriends_Yes"], "intro_01")
+combine_sequences(["amazon_demo/Introduction_MakeFriends", "amazon_demo/Personalization_BecomingFriends"], "intro_02")
+combine_sequences(["amazon_demo/Introduction_MakeFriends_Yes", "amazon_demo/Personalization_BecomingFriends"], "intro_03")
+
+combine_sequences(["happy/happy_nodding", "happy/happy_8_109"], "encouragement_01")
+combine_sequences(["happy/happy_20181204_130211"], "encouragement_02")
+combine_sequences(["yes", "sesame/sesame10"], "encouragement_03")
+combine_sequences(["happy/happy_8_109", "yes"], "encouragement_04")
+combine_sequences(["happy/happy", "sesame/sesame10"], "encouragement_05")
+combine_sequences(["sesame/sesame10", "happy/happy_nodding"], "encouragement_06")
+
+combine_sequences(["grand/grand12", "happy/happy_20181204_120338"], "end_01")
+combine_sequences(["happy/happy_daydream", "grand/grand8"], "end_02")
+combine_sequences(["grand/grand4", "happy/happy", "grand/grand10", "happy/happy"], "end_03")
