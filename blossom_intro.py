360d2d0d850dd5adc86f4f43a2138e6b57ba95ae
mariarlima <mr3418@ic.ac.uk>
2024-06-05 21:11:54 -0700

    blossom priming sequences updated

--- blossom_intro.py
+++ blossom_intro.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 
 import TTS
 from blossom_interaction import BlossomInterface
+from blossom_local_sender import BlossomLocalSender
 from config import config
 import os
 import threading
@@ -12,7 +13,10 @@ from pydub.playback import play
 load_dotenv()
 
 if config["Blossom"]["status"] == "Enabled":
-    bl = BlossomInterface()
+    if config["Blossom"]["use_network_controller"]:
+        bl = BlossomLocalSender()
+    else:
+         bl = BlossomInterface()
 
 audio_dir = "./blossom_intro_files_mp3/upd/" # changed folder with updated files 
 
@@ -39,7 +43,7 @@ audio_files = ['new_voice_1.mp3', 'new_voice_2.mp3', 'new_voice_3.mp3', 'new_voi
 
 def play_phrases_and_sequences(blossom, audio_files):
     for ind, seq in enumerate(blossom):
-        if config["Blossom"]["status"] == "Enabled":
+        if config["Blossom"]["status"] == "Enabled":        
             bl_thread = threading.Thread(
                 target=bl.do_sequence,
                 kwargs={
@@ -49,13 +53,13 @@ def play_phrases_and_sequences(blossom, audio_files):
             )
             bl_thread.start()
 
-        audio_file_path = os.path.join(audio_dir, audio_files[ind])
-        play_mp3(audio_file_path)
+            audio_file_path = os.path.join(audio_dir, audio_files[ind])
+            play_mp3(audio_file_path)
 
-        if audio_files[ind] == audio_files[1]:
-            time.sleep(5)
-        else:
-            time.sleep(1.5)
+            if audio_files[ind] == audio_files[1]:
+                time.sleep(5)
+            else:
+                time.sleep(1.5)
 
     
 play_phrases_and_sequences(blossom, audio_files)
