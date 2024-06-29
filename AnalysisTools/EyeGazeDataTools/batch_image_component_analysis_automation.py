b729bb1d738bf938c5cce1b2d2d7ffb724be382f
zhoufeiy <zhoufeiy@usc.edu>
2024-06-28 18:44:53 -0700

    Add script to batch process data, and a folder to save them.

---
+++ AnalysisTools/EyeGazeDataTools/batch_image_component_analysis_automation.py
@@ -0,0 +1,31 @@
+import os
+import re
+import csv
+from image_component_analysis import get_fixation_duration_per_image_component
+
+directory_path = "./data/"
+processed_data_path = "./data_processed/"
+
+pattern = r'(P\d{2}_S\d)\s?(#\s?)?.*?_fixations\.csv'
+
+# Get all file names in the directory
+file_names = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+
+# Print all file names
+for file_name in file_names:
+    match = re.search(pattern, file_name)
+    if match:
+        # Extracting the desired part
+        extracted_string = match.group(1).replace('_', ' ')
+        print(f"Processing: {extracted_string}")
+        print(f"File name: {file_name}")
+        data = get_fixation_duration_per_image_component(f"{directory_path}{file_name}")
+        save_file_name = f"{processed_data_path}{extracted_string.replace(' ', '_')}_image_component_analysis.csv"
+        with open(save_file_name, mode='w', newline='') as file:
+            keys = ""
+            values = ""
+            for key, item in data.items():
+                keys += key + ","
+                values += str(item) + ","
+            file.writelines(keys[:-1] + "\n")
+            file.writelines(values[:-1] + "\n")
