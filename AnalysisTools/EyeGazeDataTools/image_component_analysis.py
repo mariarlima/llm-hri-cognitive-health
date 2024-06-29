1defb3af70a55d0681f2bf75d67163c13d16b2db
zhoufeiy <zhoufeiy@usc.edu>
2024-06-28 18:25:33 -0700

    Refine image component analysis script.

--- AnalysisTools/EyeGazeDataTools/image_component_analysis.py
+++ AnalysisTools/EyeGazeDataTools/image_component_analysis.py
@@ -39,13 +39,31 @@ def get_fixation_duration_per_image_component(data_path):
     for key in annotation.keys():
         analysis_result[key] = 0.0
 
+    surroundings = 0.0
+    image_component_duration = 0.0
+
     for point in points:
         x, y, duration = point[0], point[1], point[2]
         detect_shape = point_in_polygon.detect(x, y, annotation)
         if detect_shape is not None:
             analysis_result[detect_shape] += duration
+            image_component_duration += duration
+        else:
+            surroundings += duration
 
     print(f"Analysis Result: {json.dumps(analysis_result, indent=4)}")
+    print(f"Surroundings: {surroundings}")
+    print(f"Image Components: {image_component_duration}")
+    final_result = {}
+    for key in analysis_result.keys():
+        final_result[key] = analysis_result[key]
+    final_result["Surroundings"] = surroundings
+    final_result["Image Components"] = image_component_duration
+    total_duration = surroundings + image_component_duration
+    for key in final_result.keys():
+        final_result[key] = round(final_result[key] / total_duration, 2)
+
+    print(f"Final Result: {json.dumps(final_result, indent=4)}")
     return analysis_result
 
-get_fixation_duration_per_image_component("./data/P21_S1_fixations.csv")
+# get_fixation_duration_per_image_component("./data/P21_S1_fixations.csv")
